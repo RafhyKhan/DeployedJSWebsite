784a3982ecb4adc98a3bcccb714bb0e1d5b1c0da
RafhyKhan <Rafhy.mk@hotmail.com>
2026-03-26 10:39:08 -0600

    Created Basic Script
    Made the basic functions for the script. Adding if statements necessary for scripts to run.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Rafhy Khan
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -11,6 +11,8 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+let modelname = "";
+let duration = 0;
 
 
 
@@ -27,6 +29,18 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+function recalculate() {
+    let costLabel = document.getElementById("calculated-cost");
+    let totalCost = 0;
+
+    if (modelname == "XYZ") {
+        totalCost = duration * 100
+    } else if (modelname == "CPRG") {
+        totalCost = duration * 213;
+    }
+
+    calculat
+}
 
 
 
@@ -47,7 +61,25 @@ You are encouraged to use the provided naming convention for ease of review.
 // INSERT YOUR CODE HERE
 
 
+let switchModel = 0
 
+function changeModel() {
+    let modelTextSpan = 0;
+
+    if (modelName == "XYZ") {
+        modelName = "CPRG";
+        //change inner HTML
+
+    } else if (modelName == "CPRG") {
+        modelName = "XYZ"
+        //change inner HTML
+    }
+
+
+    recalculate();
+
+    //modelButton.addEventListener("click", changeModel);
+}
 
 
 
@@ -65,5 +97,19 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+
+
+let changeDuration = 0;
+
+
+function changeDuration() {
+    let durationText = 0;
+
+    durationText = prompt("Enter the new duration: ");
+
+    recalculate(durationText);
+
+    //Attach to the button
+}
 
 
